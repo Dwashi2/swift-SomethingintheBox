33918976f5fded55dcd3da88fa1c235ce4447511
Daniel Washington <danielvertigo@hotmail.com>
2021-06-02 13:41:06 -0300

    Files

--- SomethingintheBox/ViewController.swift
+++ SomethingintheBox/ViewController.swift
@@ -5,6 +5,43 @@
 //  Created by Daniel Washington Ignacio on 02/06/21.
 //
 
+/*
+ Create a function that returns true if an asterisk * is inside a box.
+
+ Examples
+
+ inBox([
+   "###",
+   "#*#",
+   "###"
+ ]) ➞ true
+
+ inBox([
+   "####",
+   "#* #",
+   "#  #",
+   "####"
+ ]) ➞ true
+
+ inBox([
+   "*####",
+   "# #",
+   "#  #*",
+   "####"
+ ]) ➞ false
+
+ inBox([
+   "#####",
+   "#   #",
+   "#   #",
+   "#   #",
+   "#####"
+ ]) ➞ false
+ Notes
+
+ The asterisk may be in the array, however, it must be inside the box, if it exists.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +49,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(inBox([
+            "###",
+            "#*#",
+            "###"
+          ]))
     }
+    
 
+    func inBox(_ arr: [String]) -> Bool {
+        var n: Bool = false
+            for m in arr {
+                for char in m {
+                    if char == "*" {
+                        n = true
+                         if (m[m.startIndex] == "*") || (m[m.index(before: m.endIndex)] == "*") {
+                            n = false
+                            }
+                     }
+                 }
+            }
+              
+            return n
+    }
 
 }
 
